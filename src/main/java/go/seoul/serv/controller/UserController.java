505ef7a1b69f7e1f09bf0aa290ab8bf7263e84c5
LAPTOP-OGNPS07V\moons <snowball9820@gmail.com>
2024-04-04 17:30:59 +0900

    Feat: [#1] user entity, repository, controller 추가, application.properties 설정 추가
    - MySQL 데이터베이스 연결 설정을 application.properties에 추가
    - 사용자 정보를 관리하기 위한 JPA 엔티티 User와 JpaRepository를 상속받는 UserRepository를 생성
    - 사용자를 추가하고 조회하는 API 엔드포인트를 제공하는 UserController를 구현

---
+++ src/main/java/go/seoul/serv/controller/UserController.java
@@ -0,0 +1,27 @@
+package go.seoul.serv.controller;
+
+import go.seoul.serv.model.User;
+import go.seoul.serv.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping
+    public User addUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @GetMapping
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+}
+
