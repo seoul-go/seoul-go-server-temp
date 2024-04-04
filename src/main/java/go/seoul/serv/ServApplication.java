fd4e3a579a6765c698989d00b6d26790e5271e13
LAPTOP-OGNPS07V\moons <snowball9820@gmail.com>
2024-04-04 14:46:31 +0900

    Initial Commit

---
+++ src/main/java/go/seoul/serv/ServApplication.java
@@ -0,0 +1,13 @@
+package go.seoul.serv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServApplication.class, args);
+	}
+
+}
---
+++ src/test/java/go/seoul/serv/ServApplicationTests.java
@@ -0,0 +1,13 @@
+package go.seoul.serv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
