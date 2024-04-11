932d0de154d4470d7e07476ee421eeaa027bd7b4
LAPTOP-OGNPS07V\moons <snowball9820@gmail.com>
2024-04-12 06:06:44 +0900

    Feat: [#4] UserDTO 클래스 추가
    - UserDTO 클래스를 추가하여 사용자의 기본 정보(이름, 사용자 식별 아이디, 역할)를 저장할 수 있는 데이터 전송 객체를 생성함

---
+++ src/main/java/go/seoul/serv/dto/UserDTO.java
@@ -0,0 +1,12 @@
+package go.seoul.serv.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDTO {
+    private String role;
+    private String name; // 제공 받은 name
+    private String username; //리소스 서버에서 발급 받은 정보로 사용자를 특정 아이디값
+}
