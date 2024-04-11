10cd8979e28d66e394d731942fb10275a24ab143
LAPTOP-OGNPS07V\moons <snowball9820@gmail.com>
2024-04-12 07:39:51 +0900

    Feat: [#4] UserRepository 인터페이스 추가
    - UserEntity 클래스의 객체를 다루며 findByUsername 메소드를 통해 특정 사용자명을 가진 사용자를 데이터베이스에서 검색할 수 있는 기능을 제공함

--- src/main/java/go/seoul/serv/service/CustomOAuth2UserService.java
+++ src/main/java/go/seoul/serv/service/CustomOAuth2UserService.java
@@ -1,6 +1,8 @@
 package go.seoul.serv.service;
 
 import go.seoul.serv.dto.*;
+import go.seoul.serv.entity.UserEntity;
+import go.seoul.serv.repository.UserRepository;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -9,6 +11,17 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    //db UserRepository 주입
+    //필드 객체 변수 선언
+    private final UserRepository userRepository;
+
+    public CustomOAuth2UserService(UserRepository userRepository) {
+        //초기화
+        this.userRepository = userRepository;
+
+    }
+
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
@@ -33,12 +46,45 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
         //리소스 서버에서 발급 받은 정보로 사용자를 특정할 아이디값을 만듬
         String username = oAuth2Response.getProvider()+" "+oAuth2Response.getProviderId();
 
-        UserDTO userDTO = new UserDTO();
-        userDTO.setUsername(username);
-        userDTO.setName(oAuth2Response.getName());
-        userDTO.setRole("ROLE_USER");
+        //username 이 존재 하는지 조회
+        UserEntity existData = userRepository.findByUsername(username);
 
-        return new CustomOAuth2User(userDTO);
+        //한번도 존재하지 않았던 회원
+        if (existData == null) {
+            //데이터를 만들어서 넣어줌
+            UserEntity userEntity = new UserEntity();
+            userEntity.setUsername(username);
+            userEntity.setEmail(oAuth2Response.getEmail());
+            userEntity.setName(oAuth2Response.getName());
+            userEntity.setRole("ROLE_USER");
+
+            userRepository.save(userEntity);
+
+            UserDTO userDTO = new UserDTO();
+            userDTO.setUsername(username);
+            userDTO.setName(oAuth2Response.getName());
+            userDTO.setRole("ROLE_USER");
+
+            return new CustomOAuth2User(userDTO);
+
+
+        } else {
+            //이미 데이터가 존재할 때
+            //업데이트만 진행함
+            existData.setEmail(oAuth2Response.getEmail());
+            existData.setName(oAuth2Response.getName());
+
+            userRepository.save(existData);
+
+            UserDTO userDTO = new UserDTO();
+            userDTO.setUsername(existData.getUsername());
+            userDTO.setName(oAuth2Response.getName());
+            userDTO.setRole(existData.getRole());
+
+            return new CustomOAuth2User(userDTO);
+
+        }
+
     }
 
 }
