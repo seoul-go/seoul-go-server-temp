29efb3301c125594a2c05ee90b7e40999b510eef
LAPTOP-OGNPS07V\moons <snowball9820@gmail.com>
2024-04-12 01:01:37 +0900

    Feat: [#4] OAuth2 사용자 정보를 위한 DTO 인터페이스 추가
    - OAuth2 인증 과정에서 얻은 사용자 정보(제공자, 제공자 ID, 이메일, 이름)를 위한OAuth2Response 인터페이스를 추가

--- src/main/java/go/seoul/serv/dto/OAuth2Response.java
+++ src/main/java/go/seoul/serv/dto/OAuth2Response.java
@@ -1,4 +1,15 @@
 package go.seoul.serv.dto;
 
-public class OAuth2Response {
+public interface OAuth2Response {
+
+    //제공자
+    String getProvider();
+    //제공자에서 발급해주는 ID(번호)
+    String getProviderId();
+    //사용자 이메일
+    String getEmail();
+    //사용자 이름
+    String getName();
+
+
 }
