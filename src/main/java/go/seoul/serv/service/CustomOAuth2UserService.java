d5e11095bbd200263d32eaa66a887925d494d80d
LAPTOP-OGNPS07V\moons <snowball9820@gmail.com>
2024-04-12 05:37:12 +0900

    Feat: [#4] 구글 소셜 로그인 사용자 정보 처리를 위한 DTO 클래스 추가
    - 구글 소셜 로그인을 통해 얻은 사용자 정보를 처리하기 위한 GoogleResponse DTO 클래스를 추가함
    - OAuth2Response 인터페이스를 구현하며, 구글 API로부터 받은 사용자 데이터(제공자 ID, 이메일, 이름)를 매핑하는 역할을 생성 함
    -생성자를 통해 구글 API의 응답 데이터를 직접적으로 할당하고, 각 필요한 정보를 반환하는 메서드를 구현함

--- src/main/java/go/seoul/serv/service/CustomOAuth2UserService.java
+++ src/main/java/go/seoul/serv/service/CustomOAuth2UserService.java
@@ -1,25 +1,32 @@
 package go.seoul.serv.service;
 
+import go.seoul.serv.dto.GoogleResponse;
+import go.seoul.serv.dto.NaverResponse;
+import go.seoul.serv.dto.OAuth2Response;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
 
-public class OAuth2UserService extends DefaultOAuth2UserService {
+@Service
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
 
-        OAuth2User oAuth2User=super.loadUser(userRequest);
+        OAuth2User oAuth2User = super.loadUser(userRequest);
 
         //registration 에서 넘어온 값 출처 확인
         String registrationId = userRequest.getClientRegistration().getRegistrationId();
-
+        OAuth2Response oAuth2Response = null;
         if (registrationId.equals("naver")) {
             //네이버 데이터는 json 방식, response key 내부에 정보 존재
+            oAuth2Response = new NaverResponse(oAuth2User.getAttributes());
 
         } else if (registrationId.equals("google")) {
             //구글은 json 외부에 정보가 존재
+            oAuth2Response = new GoogleResponse(oAuth2User.getAttributes());
 
         } else {
             return null;
