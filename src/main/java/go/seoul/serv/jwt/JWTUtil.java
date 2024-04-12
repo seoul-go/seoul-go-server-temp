0d4c57c2e77457051c16b2fe93602f5302f33760
LAPTOP-OGNPS07V\moons <snowball9820@gmail.com>
2024-04-12 15:10:09 +0900

    Feat: [#8] JWT 처리를 위한 JWTUtil 클래스 추가
    - @Component 어노테이션을 사용하여 스프링 컴포넌트로 등록되고 설정 파일에서 JWT 비밀키를 로드함
    - 클래스에는 JWT 생성, 사용자 이름과 역할 정보 추출, 토큰 만료 검증 등의 메서드를 구현함

---
+++ src/main/java/go/seoul/serv/jwt/JWTUtil.java
@@ -0,0 +1,50 @@
+package go.seoul.serv.jwt;
+
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+//JWTUtil : 0.12.3
+@Component //springboot 에서 component 로 관리되기 위한 annotation
+public class JWTUtil {
+    private SecretKey secretKey;
+
+    //생성자+@Value로 yml에 있는 특정 값 가져오기
+    public JWTUtil(@Value("${spring.jwt.secret}") String secret) {
+        //초기화
+        //UTF_8 방식으로 인코딩 변경, JWT 암호화 H256 사용
+        secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
+    }
+    //token 검증 메서드
+    public String getUsername(String token) {
+
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("username", String.class);
+    }
+
+    public String getRole(String token) {
+
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("role", String.class);
+    }
+
+    public Boolean isExpired(String token) {
+
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getExpiration().before(new Date());
+    }
+
+    public String createJwt(String username, String role, Long expiredMs) {
+
+        return Jwts.builder()
+                .claim("username", username)
+                .claim("role", role)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expiredMs))
+                .signWith(secretKey)
+                .compact();
+    }
+
+}
