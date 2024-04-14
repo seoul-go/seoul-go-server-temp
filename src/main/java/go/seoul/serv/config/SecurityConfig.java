ca3e7213f808ed22946b0ece313de6bd5aeac708
LAPTOP-OGNPS07V\moons <snowball9820@gmail.com>
2024-04-14 23:09:01 +0900

    Feat:[#10] SecurityConfig 클래스 추가 및 보안 설정 구현
    -
    SecurityConfig 클래스를 추가하여 애플리케이션의 보안 설정을 구성함

--- src/main/java/go/seoul/serv/config/SecurityConfig.java
+++ src/main/java/go/seoul/serv/config/SecurityConfig.java
@@ -1,57 +1,34 @@
 package go.seoul.serv.config;
 
-import go.seoul.serv.service.CustomOAuth2UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration //configuration 으로 등록을 위한 annotation
 @EnableWebSecurity //component 스캔을 위한 annotation
 public class SecurityConfig {
-    //SecurityFilterChain interface 를 return 하는 메서드 생성
-
-    private final CustomOAuth2UserService customOAuth2UserService;
-
-    public SecurityConfig(CustomOAuth2UserService customOAuth2UserService) {
-
-        this.customOAuth2UserService = customOAuth2UserService;
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-
-        //csrf disable
         http
-                .csrf((auth) -> auth.disable());//csrf 공격을 방어하는 메서드, jwt 발급 후 stateless 상태로 관리하기 때문에 꺼놓음
-
-        //From 로그인 방식-disable
-        http
-                .formLogin((auth) -> auth.disable());//jwt, OAuth 방식으로 로그인을 하기 때문에 From 방식 꺼놓음
-
-        //HTTP Basic 인증 방식-disable
-        http
-                .httpBasic((auth) -> auth.disable());//jwt, OAuth 방식으로 로그인을 하기 때문에 Http 방식 꺼놓음
-
-        //oauth2
-        http
-                .oauth2Login((oauth2) -> oauth2
-                        .userInfoEndpoint((userInfoEndpointConfig) -> userInfoEndpointConfig
-                                .userService(customOAuth2UserService)));//커스텀 구현은 추후에, 현재는 default 설정
-
-        //경로별 인가 작업
-        http
-                .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/").permitAll()//route 경로는 permit all
-                        .anyRequest().authenticated());//나머지 경로는 로그인한 사용자만 접근하도록 authenticated
-
-        //세션 설정-stateless
-        http
-                .sessionManagement((session) -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));//jwt로 인증 인가를 진행하기 때문에 STATELESS
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/v1/users/join", "/v1/login").permitAll()
+                        .anyRequest().authenticated())
+                .httpBasic(httpBasic -> httpBasic.disable())
+                .formLogin(formLogin -> formLogin.disable())
+                .sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
