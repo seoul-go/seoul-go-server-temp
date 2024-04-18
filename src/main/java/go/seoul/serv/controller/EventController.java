423272b44f4d14a920ca4ffcf864960fbcd69b2c
LAPTOP-OGNPS07V\moons <snowball9820@gmail.com>
2024-04-18 17:48:44 +0900

    Feat: [#13] 이벤트 조회 API 엔드포인트 추가
    - API 엔드포인트 두 개를 추가하여 클라이언트가 모든 이벤트를 조회하거나 특정 ID를 가진 이벤트를 조회할 수 있도록 구현함
    - ID에 해당하는 이벤트가 없을 경우 'ResourceNotFoundException'을 통해 예외 처리를 하여 클라이언트에게 적절한 오류 메시지를 전달함

---
+++ src/main/java/go/seoul/serv/controller/EventController.java
@@ -0,0 +1,33 @@
+package go.seoul.serv.controller;
+
+import go.seoul.serv.entity.Event;
+import go.seoul.serv.exception.ResourceNotFoundException;
+import go.seoul.serv.repository.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/events")
+public class EventController {
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    // 클라이언트가 모든 이벤트를 불러올 수 있는 API
+    @GetMapping
+    public List<Event> getAllEvents() {
+        return eventRepository.findAll();
+    }
+
+    // ID에 해당하는 단일 이벤트를 불러오는 API
+    @GetMapping("/api/events/{id}")
+    public Event getEventById(@PathVariable Long id) {
+        return eventRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Event not found with id " + id));
+    }
+}
