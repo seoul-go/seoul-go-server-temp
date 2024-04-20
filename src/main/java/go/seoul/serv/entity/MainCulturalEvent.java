f52f524c75bb56903f44bcf6bc5a996fda020794
LAPTOP-OGNPS07V\moons <snowball9820@gmail.com>
2024-04-20 16:18:34 +0900

    Feat: [#15] Add JPA annotations to MainCulturalEvent entity
    - @Table, @Column, @GeneratedValue 어노테이션을 MainCulturalEvent 클래스에 추가함
    - MainCulturalEvent 클래스에 JPA 어노테이션을 추가하여 데이터베이스 필드와의 매핑을 명확히 함

--- src/main/java/go/seoul/serv/entity/MainCulturalEvent.java
+++ src/main/java/go/seoul/serv/entity/MainCulturalEvent.java
@@ -1,20 +1,35 @@
 package go.seoul.serv.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import jakarta.persistence.Id;
+
 @Getter
 @Setter
 @Entity
+@Table(name = "main_cultural_event")
 public class MainCulturalEvent {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 데이터베이스에서 ID가 자동으로 생성되는 경우 사용
+    @Column(name = "main_cultural_event_id") // 데이터베이스 컬럼 이름 명시
     private int mainCulturalEventId;
+
+    @Column(name = "codename")
     private String codename;
+
+    @Column(name = "guname")
     private String guname;
+
+    @Column(name = "title")
     private String title;
+
+    @Column(name = "date")
     private String date;
+
+    @Column(name = "place")
     private String place;
+
+    @Column(name = "main_image")
     private String mainImage;
 
 }
