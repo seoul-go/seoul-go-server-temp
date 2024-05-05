51251f051a01bfae9758ad49a0f8a783fe707594
LAPTOP-OGNPS07V\moons <snowball9820@gmail.com>
2024-05-06 01:06:33 +0900

    Refactor: [#26] 쿼리 방식을 JPQL로 변경하고 @Query 어노테이션 적용
    - 기존의 메서드들(findAllByEndDateBetween, findAllByStartDateBetween)을 JPQL을 사용하는 방식으로 리팩토링함
    -JPQL을 사용하여 명시적으로 엔티티와 필드 이름을 쿼리에 포함시키고, @Query 어노테이션을 통해 쿼리를 직접 정의함으로써 쿼리의 가독성과 유지보수성을 더욱 향상함

--- src/main/java/go/seoul/serv/repository/MainCulturalEventRepository.java
+++ src/main/java/go/seoul/serv/repository/MainCulturalEventRepository.java
@@ -2,11 +2,17 @@ package go.seoul.serv.repository;
 
 import go.seoul.serv.entity.MainCulturalEventEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Date;
 import java.util.List;
 
 public interface MainCulturalEventRepository extends JpaRepository<MainCulturalEventEntity, Integer> {
-    List<MainCulturalEventEntity> findAllByEndDateBetween(Date start, Date end);
-    List<MainCulturalEventEntity> findAllByStartDateBetween(Date start, Date end);
+    // 한 달 이내에 종료되는 행사 조회
+    @Query("SELECT m FROM MainCulturalEventEntity m WHERE m.endDate BETWEEN :start AND :end")
+    List<MainCulturalEventEntity> findEventsEndingWithinAMonth(Date start, Date end);
+
+    // 최근 2주 이내에 시작된 행사 조회
+    @Query("SELECT m FROM MainCulturalEventEntity m WHERE m.startDate BETWEEN :start AND :end")
+    List<MainCulturalEventEntity> findEventsStartedWithinTwoWeeks(Date start, Date end);
 }
