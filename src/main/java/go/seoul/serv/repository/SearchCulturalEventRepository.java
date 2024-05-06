ed41d28d3924c329362f1b48daf40102aaadd9a7
LAPTOP-OGNPS07V\moons <snowball9820@gmail.com>
2024-05-07 03:08:29 +0900

    Feat: [#29] codename과 오늘 날짜를 기준으로 활성 행사 조회 기능 추가
    - codename을 파라미터로 받아, 해당 codename의 행사 중 오늘 날짜 이후에 종료되는 행사만 조회하는 JPA 쿼리 메소드를 SearchCulturalEventRepository 인터페이스에 추가함

--- src/main/java/go/seoul/serv/repository/SearchCulturalEventRepository.java
+++ src/main/java/go/seoul/serv/repository/SearchCulturalEventRepository.java
@@ -1,7 +1,14 @@
 package go.seoul.serv.repository;
 
 import go.seoul.serv.entity.SearchCulturalEventEntity;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Date;
+import java.util.List;
 
 public interface SearchCulturalEventRepository extends JpaRepository<SearchCulturalEventEntity, Integer> {
+    @Query("SELECT s FROM SearchCulturalEventEntity s WHERE s.codename = :codename AND s.endDate > :today")
+    List<SearchCulturalEventEntity> findActiveEventsByCodename(@Param("codename") String codename, @Param("today") Date today);
 }
